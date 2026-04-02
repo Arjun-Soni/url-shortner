2c512aff98d6c2c5885b5d783b64dd59660ab6ab
Arjun-Soni <asoni9524@gmail.com>
2026-04-02 20:42:37 +0530

    added redirection method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import random, string
+from fastapi.responses import RedirectResponse
+from fastapi import HTTPException
 app = FastAPI()
 url_map = {}
 
@@ -13,11 +15,20 @@ def generate_random_code(length=6):
 @app.get("/")
 async def hello():
     return{"message":"hello"}
+
 @app.post("/shorten")
 async def shorten_url(item: URLItem):
     code = generate_random_code()
+    if not item.url.startswith("http"):
+        raise HTTPException(status_code=400, detail="Invalid URL")
     while code in url_map:
         code = generate_random_code()
     url_map[code] = item.url
     return {"short_url": f"http://127.0.0.1:8000/{code}"}
-    
+
+@app.get("/{code}")
+async def redirect_url(code: str):
+    if code in url_map:
+        return RedirectResponse(url=url_map[code], status_code=302)
+    else:
+        raise HTTPException(status_code=404, detail="URL not found")
